c1a633113e0a12652c83c88d28148f79d85d5f0f
Rick <givemepassxd999@gmail.com>
2017-07-14 14:12:00 +0800

    first commit

---
+++ app/src/main/java/example/rick/tablayoutdemo/MainActivity.java
@@ -0,0 +1,67 @@
+package example.rick.tablayoutdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private android.support.design.widget.TabLayout mTabs;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTabs = (android.support.design.widget.TabLayout) findViewById(R.id.tabs);
+        mTabs.addTab(mTabs.newTab().setText("Tab 1"));
+        mTabs.addTab(mTabs.newTab().setText("Tab 2"));
+        mTabs.addTab(mTabs.newTab().setText("Tab 3"));
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new SamplePagerAdapter());
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabs));
+    }
+
+    class SamplePagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Item " + (position + 1);
+        }
+
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = getLayoutInflater().inflate(R.layout.pager_item,
+                    container, false);
+            container.addView(view);
+            TextView title = (TextView) view.findViewById(R.id.item_title);
+            title.setText(String.valueOf(position + 1));
+            return view;
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+    }
+
+}
